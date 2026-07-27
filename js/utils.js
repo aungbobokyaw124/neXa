a23c8bf33f2837bec5536eb2ff30fa5b2225328e
aungbobokyaw.com <141192688+aungbobokyaw124@users.noreply.github.com>
2026-07-27 09:03:41 +0630

    Add utility functions for element selection and sleep
    Added utility functions for element selection and delay.

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,10 @@
+/* ==========================================
+   neXa Utilities
+   ========================================== */
 
+// Element ရွေးချယ်ရန် Helper
+const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
+
+// Delay ပြုလုပ်ရန် Helper
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
